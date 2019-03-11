dd52cffbbbd2c967707c28091d2251053a3a1763
Antonia Lechner <antonia.lechner@diffblue.com>
2019-03-11 12:36:03 +0000

    Move det creation code to new file

---
+++ jbmc/src/java_bytecode/java_det_object_factory.h
@@ -0,0 +1,20 @@
+#ifndef CPROVER_JAVA_BYTECODE_JAVA_DET_OBJECT_FACTORY_H
+#define CPROVER_JAVA_BYTECODE_JAVA_DET_OBJECT_FACTORY_H
+
+#include "java_bytecode_language.h"
+#include "select_pointer_type.h"
+
+#include <util/allocate_objects.h>
+#include <util/message.h>
+#include <util/std_code.h>
+#include <util/symbol_table.h>
+
+void static_assignments_from_json(
+  const jsont &json,
+  const symbol_exprt &expr,
+  const irep_idt &class_name,
+  code_blockt &init_body,
+  symbol_table_baset &symbol_table,
+  const source_locationt &loc);
+
+#endif //CPROVER_JAVA_BYTECODE_JAVA_DET_OBJECT_FACTORY_H
